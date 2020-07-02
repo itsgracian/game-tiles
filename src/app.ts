7a87ad32aaf6ab867f967e646133a532b354148d
itsgracian <gracian2020@gmail.com>
2020-07-02 10:13:04 +0200

    setup prettier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 
-const app:Application = express();
+const app: Application = express();
 // setup body parser
 app.use(bodyParser.json());
 
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import app from './app';
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3400;
 
-server.listen(PORT,()=>{
-    console.log(`Server started on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
 export default server;
