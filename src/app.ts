64f7c464e98e139b73277a37ba9af9cb596cf32e
itsgracian <gracian2020@gmail.com>
2020-07-02 10:15:24 +0200

    setup morgan

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app: Application = express();
+
+// setup morgan
+app.use(morgan('dev'));
+
 // setup body parser
 app.use(bodyParser.json());
 
