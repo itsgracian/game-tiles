ddf82699cd5c6c7a9c2f7b87787cca5fad857310
itsgracian <gracian2020@gmail.com>
2020-07-02 09:51:54 +0200

    express server initialized

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+
+const app:Application = express();
+
+// setup body parser
+app.use(bodyParser.json());
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import http from 'http';
+import app from './app';
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3400;
+
+server.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`);
+});
+
+export default server;
